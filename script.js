31371ade7affc7a64553a985326110a020bdbdd7
gelaterijabrstowc-ops <gelaterijabrstowc@gmail.com>
2026-07-18 22:09:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -86,3 +86,33 @@ cards.forEach(card => {
 });
 
 console.log("🍦 Gelaterija Brstowc uspešno naložena!");
+
+/* ==========================
+   TEMNI NAČIN
+========================== */
+
+const darkModeButton = document.getElementById("darkMode");
+
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark");
+}
+
+if (darkModeButton) {
+
+    darkModeButton.addEventListener("click", () => {
+
+        document.body.classList.toggle("dark");
+
+        if (document.body.classList.contains("dark")) {
+
+            localStorage.setItem("theme", "dark");
+
+        } else {
+
+            localStorage.setItem("theme", "light");
+
+        }
+
+    });
+
+}
