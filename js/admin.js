1d1ec81d64574112c563dc2f7344a30eed3bc5e4
gelaterijabrstowc-ops <gelaterijabrstowc@gmail.com>
2026-07-19 23:40:08 +0200

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,1 +1,157 @@
+// ==========================================
+// GELATERIJA BRSTOWC
+// ADMIN - NAROČILA
+// ==========================================
 
+
+import { db } from "./firebase.js";
+
+
+import {
+    collection,
+    getDocs,
+    orderBy,
+    query
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+
+
+const adminOrders =
+document.getElementById("adminOrders");
+
+
+
+
+async function loadOrders(){
+
+
+    if(!adminOrders) return;
+
+
+
+    try{
+
+
+        const q = query(
+
+            collection(db,"orders"),
+
+            orderBy(
+                "createdAt",
+                "desc"
+            )
+
+        );
+
+
+
+        const snapshot =
+        await getDocs(q);
+
+
+
+        adminOrders.innerHTML="";
+
+
+
+        if(snapshot.empty){
+
+
+            adminOrders.innerHTML =
+            "<p>Ni naročil.</p>";
+
+
+            return;
+
+        }
+
+
+
+        snapshot.forEach((doc)=>{
+
+
+            const order =
+            doc.data();
+
+
+
+            adminOrders.innerHTML += `
+
+
+            <div class="admin-order">
+
+
+                <h3>
+                📦 Naročilo
+                </h3>
+
+
+                <p>
+                👤 ${order.customerName}
+                </p>
+
+
+                <p>
+                📞 ${order.phone}
+                </p>
+
+
+                <p>
+                📍 ${order.address}
+                </p>
+
+
+
+                <p>
+                🍦
+
+                ${order.products.map(
+
+                    item =>
+
+                    item.name +
+                    " x" +
+                    item.quantity
+
+                ).join(", ")}
+
+                </p>
+
+
+
+                <p>
+                💰
+
+                ${order.total.toFixed(2)} €
+
+                </p>
+
+
+            </div>
+
+
+            `;
+
+
+        });
+
+
+
+    }
+    catch(error){
+
+
+        console.error(
+            "Napaka pri nalaganju naročil:",
+            error
+        );
+
+
+    }
+
+
+}
+
+
+
+loadOrders();
