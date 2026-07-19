b90b6cf95e7cd6e5eaaca51de9ebec50c45fde01
gelaterijabrstowc-ops <gelaterijabrstowc@gmail.com>
2026-07-19 23:46:43 +0200

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -3,6 +3,16 @@
 // ADMIN - NAROČILA
 // ==========================================
 
+import { auth, db } from "./firebase.js";
+
+import {
+    onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+import {
+    doc,
+    getDoc
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 import { db } from "./firebase.js";
 
